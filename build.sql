cb1418a3d03377ee531325bbfe5ad7875a91fd23
popcat <moviemaker7@gmail.com>
2015-10-20 21:28:48 -0400

    added db schema build script

---
+++ build.sql
@@ -0,0 +1,109 @@
+-- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: telesked
+-- ------------------------------------------------------
+-- Server version	5.5.44-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `CALL`
+--
+
+DROP TABLE IF EXISTS `CALL`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `CALL` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PHONE` varchar(12) NOT NULL,
+  `FNAME` varchar(26) NOT NULL,
+  `LNAME` varchar(26) NOT NULL,
+  `POSITION` text NOT NULL,
+  `DESCRIPTION` text NOT NULL,
+  `STATUS` int(11) NOT NULL,
+  `REP_ID` int(11) NOT NULL,
+  `CALL_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  KEY `CALL_REP` (`REP_ID`),
+  KEY `CALL_STATUS` (`STATUS`),
+  CONSTRAINT `CALL_STATUS` FOREIGN KEY (`STATUS`) REFERENCES `STATUS` (`ID`),
+  CONSTRAINT `CALL_REP` FOREIGN KEY (`REP_ID`) REFERENCES `USER` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CALL`
+--
+
+LOCK TABLES `CALL` WRITE;
+/*!40000 ALTER TABLE `CALL` DISABLE KEYS */;
+/*!40000 ALTER TABLE `CALL` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `STATUS`
+--
+
+DROP TABLE IF EXISTS `STATUS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `STATUS` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `STATUS` varchar(12) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `STATUS`
+--
+
+LOCK TABLES `STATUS` WRITE;
+/*!40000 ALTER TABLE `STATUS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `STATUS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `USER`
+--
+
+DROP TABLE IF EXISTS `USER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `USER` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `USER`
+--
+
+LOCK TABLES `USER` WRITE;
+/*!40000 ALTER TABLE `USER` DISABLE KEYS */;
+/*!40000 ALTER TABLE `USER` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-10-21  1:22:10
